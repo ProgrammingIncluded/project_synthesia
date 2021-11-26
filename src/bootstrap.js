2b1ebb2207773ee8e573f81f10e33a1af968aae0
Charles Chen <chencharlesh@gmail.com>
2021-11-25 16:55:02 -0800

    Add proper playarea resizing using virtual screen

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,11 +3,15 @@ import { Howl as G_HOWL } from "howler";
 import * as G_PIXI from "pixi.js";
 import { G_LOGGER } from "./logger.js";
 
+// Render options
+G_PIXI.settings.SCALE_MODE = G_PIXI.SCALE_MODES.NEAREST;
+
 // Load application window
 // Create the application helper and add its render target to the page
-// TODO: Dynamic height adjustment
-const G_PIXI_APP = new G_PIXI.Application({ width: 640, height: 360});
-document.body.appendChild(G_PIXI_APP.view);
+let targetDOM = document.getElementById("render");
+let targetSize = {resizeTo: targetDOM, antialias: false, autoDensity: true};
+const G_PIXI_APP = new G_PIXI.Application(targetSize);
+targetDOM.appendChild(G_PIXI_APP.view);
 
 export {
     G_HOWL,
